5a6ef2006652ce60124ab08df349f4b1d876d6cb
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-11 21:14:37 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,19 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum 250 }
   validates :summary, length: { maximum 250 }
-  validates :category, inclusion: { Fiction, Non-Fiction }
+  validates :category, presence: true, inclusion: { :in => %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
+ 
+   CLICKBAIT_WORDS = [
+    /Won't Believe/i,
+     /Secret/i,
+     /Top [0-9]*/i,
+     /Guess/i
+   ]
+ 
+   def is_clickbait?
+     if CLICKBAIT_WORDS.none? {|w| w.match title}
+       errors.add(:title, "Must be clickbait")
+     end
+   end
 end
